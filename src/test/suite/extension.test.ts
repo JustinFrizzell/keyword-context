411e4b0940f93c1a116fb91091cb5ced4177b431
Justin Frizzell <143661894+JustinFrizzell@users.noreply.github.com>
2023-09-08 19:38:47 +0100

    Added multiple tooltip test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -27,4 +27,15 @@ suite("Extension Test Suite", () => {
     assert(matchedTooltips.length === 1);
     assert(matchedTooltips[0].tooltip === "This is a test tooltip");
   });
+
+  test("Find multiple Tooltips By Keyword", () => {
+    const mockTooltips = [
+      { keyword: "test", tooltip: "This is a test tooltip" },
+      { keyword: "test", tooltip: "This is an example tooltip" },
+    ];
+
+    const matchedTooltips = findTooltipsByKeyword("test", mockTooltips);
+    assert(matchedTooltips.length === 2);
+    assert(matchedTooltips[1].tooltip === "This is an example tooltip");
+  });
 });
